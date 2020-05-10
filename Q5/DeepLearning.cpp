bd931f455b99c7d48fca216f11842f93010ad4c7
brittasrivas <47535496+brittasrivas@users.noreply.github.com>
2020-05-10 18:56:14 +0100

    q5 continued

--- Q5/DeepLearning.cpp
+++ Q5/DeepLearning.cpp
@@ -3,6 +3,18 @@
 #include <vector>
 #include <stdlib.h> //used for rand()
 
+void printMatrix(const std::vector< std::vector<double> > M, const int r, const int c)
+{
+  for (int i=0; i<r; i++)
+  {
+    for (int j=0; j<c; j++)
+    {
+      std::cout << M[i][j] << "\t";
+    }
+    std::cout << "\n";
+  }
+}
+
 void initialiseData(double* x1, double* x2, double* y1, double* y2)
 {
   x1[0] = 0.1;
@@ -35,20 +47,55 @@ void initialiseData(double* x1, double* x2, double* y1, double* y2)
 
 }
 
-void initialiseParams(double W2[2][2], double W3[3][2], double W4[2][3],
+void initialiseParams(std::vector< std::vector<double> > &W2,
+  std::vector< std::vector<double> > &W3,
+  std::vector< std::vector<double> > &W4,
   double *b2, double *b3, double *b4)
 {
-  W2[0][0] = rand() % 5000;
-  W2[0][1] = ;
-  W2[1][0] = ;
-  W2[1][1] = ;
+  srand((unsigned)time(NULL));
 
+  //Initialise W2
+  for(int i=0; i<2; i++)
+  {
+    for(int j=0; j<2; j++)
+    {
+      W2[i][j] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+    }
+  }
+  //Initialise W3
+  for(int i=0; i<3; i++)
+  {
+    for(int j=0; j<2; j++)
+    {
+      W3[i][j] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+    }
+  }
+  //Initialise W4
+  for(int i=0; i<2; i++)
+  {
+    for(int j=0; j<3; j++)
+    {
+      W4[i][j] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+    }
+  }
+
+
+  b2[0] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  b2[1] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+
+  b3[0] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  b3[1] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  b3[2] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+
+  b4[0] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  b4[1] = (double)(rand()) / (double)(RAND_MAX + 1.0);
 
 }
 
 
 int main(int argc, char* argv[])
 {
+  //INITIALISE DATA
   int N = 10; // No. of data points
 
   double *x1, *x2, *y1, *y2;
@@ -59,11 +106,57 @@ int main(int argc, char* argv[])
 
   initialiseData(x1, x2, y1, y2);
 
-  double W2[2][2], W3[3][2], W4[2][3];
+  //INITIALISE PARAMETERS
+  //double W2[2][2], W3[3][2], W4[2][3]; //TODO REMOVE
+  std::vector< std::vector<double> > W2(2, std::vector<double>(2));
+  std::vector< std::vector<double> > W3(3, std::vector<double>(2));
+  std::vector< std::vector<double> > W4(2, std::vector<double>(3));
   double *b2, *b3, *b4;
   b2 = new double[2];
   b3 = new double[3];
   b4 = new double[2];
+
+  initialiseParams(W2, W3, W4, b2, b3, b4);
+
+  //TODO REMOVE PRINTS
+  std::cout << "\nW2: \n";
+  printMatrix(W2, 2, 2);
+  std::cout << "\nW3: \n";
+  printMatrix(W3, 3, 2);
+  std::cout << "\nW4: \n";
+  printMatrix(W4, 2, 3);
+
+
+  double eta = 0.05;
+  double Niter = 1e6;
+
+  //Initialise arrays for auxiliary steps in SGD
+  double *costs_x, *x, *y;
+  double *a2, *a3, *a4;
+  double *delta1, *delta2, *delta3;
+
+  costs_x = new double[Niter];
+  x = new double[2];
+  y = new double[2];
+  //TODO ALLOCATE MEMORY FOR A AND DELTAS
+
+
+  //STOCHASTIC GRADIENT Descent
+  int random;
+  srand((unsigned)time(NULL));
+
+  for (int k=0; k<Niter; k++)
+  {
+    random = rand() % N;
+    x[0] = x1[random];
+    x[1] = x2[random];
+    y[0] = y1[random];
+    y[1] = y2[random];
+
+
+  }
+
+
 
 
 
@@ -72,6 +165,13 @@ int main(int argc, char* argv[])
   delete[] x2;
   delete[] y1;
   delete[] y2;
+  delete[] b2;
+  delete[] b3;
+  delete[] b4;
+  delete[] costs_x;
+  delete[] x;
+  delete[] y;
+  //Note: do not need to delete matrices - std::vector destructor will clear them.
 
   return 0;
 }
--- Q5/rough-work.cpp
+++ Q5/rough-work.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <stdlib.h> //used for rand()
+#include <vector>
 
 
-void fillMatrix(double M[2][2])
+void fillMatrix(std::vector< std::vector<double> > &M)
 {
-  M[0][0] = rand() / (RAND_MAX + 1.0);
-  M[0][1] = rand() / (RAND_MAX + 1.0);
-  M[1][0] = rand() / (RAND_MAX + 1.0);
-  M[1][1] = rand() / (RAND_MAX + 1.0);
+  srand((unsigned)time(NULL));
+
+  // M[0][0] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  // M[0][1] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  // M[1][0] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+  // M[1][1] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+
+  for(int i=0; i<2; i++)
+  {
+    for(int j=0; j<2; j++)
+    {
+      M[i][j] = (double)(rand()) / (double)(RAND_MAX + 1.0);
+    }
+  }
 
 }
 
@@ -15,7 +26,9 @@ void fillMatrix(double M[2][2])
 int main(int argc, char* argv[])
 {
 
-  double M[2][2];
+  //double M[2][2];
+
+  std::vector< std::vector<double> > M(2, std::vector<double>(2));
 
   fillMatrix(M);
 
@@ -26,6 +39,12 @@ int main(int argc, char* argv[])
       std::cout << M[i][j] << "\n";
     }
   }
+
+  std::cout << "Size of matrix: " << M.size() << "\n";
+
+  int number = 1e6;
+  std::cout << number << "\n";
+
 
   return 0;
 }
